ac15fc520098057791af9a3f8053a2d437232a29
Baha-Abdelatif <baha.abdelatif@gmail.com>
2021-08-04 01:13:31 +0200

    Display form for add an aliment

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ const mainDiv = document.querySelector(".main");
 const apiUrl = "http://localhost:1337";
 let allFood = [];
 
+const addFoodForm = document.forms.addFood;
+const foodTitle = addFoodForm.foodTitle;
+const expirationDateField = addFoodForm.expirationDate;
+
+addFoodForm.addEventListener("submit", addFood);
+
 const init = () => {
   getFood();
 }
-const getFood = () => {
+function getFood() {
   fetch(`${apiUrl}/fooditems?_sort=Expiration_date:ASC`)
     .then(data => data.json())
     .then(result => {
@@ -14,7 +20,7 @@ const getFood = () => {
     })
     .catch(err => { console.error(err) })
 }
-const renderFood = (food) => {
+function renderFood(food) {
   let foodList = [];
   food.forEach(f => {
     const foodItem = `<li>${f.title}</li>`;
@@ -22,4 +28,14 @@ const renderFood = (food) => {
   })
   mainDiv.innerHTML = `<h2>Liste des plats :</h2><ul>${foodList.join("")}</ul>`
 }
+function addFood(e) {
+  // Liste les propriétés disponibles de l'event
+  // console.dir(e.target)
+
+  e.preventDefault();
+  const title = foodTitle.value.trim();
+  const expirationDate = expirationDateField.value
+  console.log(title, expirationDate)
+}
+
 init();
